0ea117766071dcece85b19658f396dcda05e1e08
mirakhmadov <mukhammadazizmirakhmadov@gmail.com>
2025-03-27 14:01:18 +0500

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <iframe className="w-screen h-screen" src="https://mirahmadov.github.io/mirahmadov" />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold">mirahmadov</h1>
+    </div>
   );
 }
