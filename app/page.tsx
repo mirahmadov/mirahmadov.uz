811c2db0d9149b5e539aca1a10a17f184b2848f5
macbookpro <mukhammadazizmirakhmadov@gmail.com>
2025-12-11 12:11:19 +0500

    easysale iframe icon

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         {/* Projects */}
         <DesktopIconFrame image='/projects/nexura.png' title='NEXURA' url='https://nexura.uz' font='font-havelock' />
         <DesktopIconFrame image='/projects/techvision.png' title='TECHVISION' url='https://tech-vision.uz' />
+        <DesktopIconFrame image='/projects/easysale.png' title='EasySale' url='https://easysale.uz' />
         <DesktopIconFrame image='/projects/midea.png' title='Midea' url='https://midea-kitchen.uz/ru' />
         <DesktopIconFrame image='/projects/endo.png' title='Endo Technologies' url='https://endo-tech.uz' />
 
